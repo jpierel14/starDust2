a3f3e3476a6eceef86d9b73d0f7e2f94dd9a6d57
Justin Pierel <jpierel@stsci.edu>
2023-01-19 18:59:26 -0500

    init

--- stardust/__init__.py
+++ stardust/__init__.py
@@ -4,6 +4,7 @@
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
 from ._astropy_init import *
+from stardust import *
 # ----------------------------------------------------------------------------
 
 if not _ASTROPY_SETUP_:
